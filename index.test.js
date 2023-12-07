bf37743bf758bab7656e41099c1e0cd6433008d2
GabyAM <gdgabyam@gmail.com>
2023-12-07 08:29:42 -0300

    Writes tests for a capitalize function
    This tests cover the most important cases, and if the function passes them, it should work properly

---
+++ index.test.js
@@ -0,0 +1,12 @@
+
+it("capitalize function works", () => {
+	expect(capitalize("word")).toBe("Word");
+});
+
+it("capitalize function works when only the first character is a letter", () => {
+	expect(capitalize("a1*/")).toBe("A1*/");
+});
+
+it("capitalize function fails when input is not a word", () => {
+	expect(() => capitalize(123)).toThrow("Input a valid string");
+});
