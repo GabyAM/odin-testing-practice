f005afc67486b4687f2fb9602dc3100ee1ed14ba
GabyAM <gdgabyam@gmail.com>
2023-12-07 08:53:33 -0300

    tests reverseString function
    tests the function when a string is passed and when no string is passed

--- index.test.js
+++ index.test.js
@@ -11,3 +11,11 @@ it("capitalize function works when only the first character is a letter", () => 
 it("capitalize function fails when input is not a word", () => {
 	expect(() => capitalize(123)).toThrow("Input a valid string");
 });
+
+it("reverseString function works", () => {
+	expect(reverseString("live")).toBe("evil");
+});
+
+it("reverseString function fails when input is not string", () => {
+	expect(() => reverseString(123)).toThrow("Input a valid string");
+});
